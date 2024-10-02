e802e686317305c7851c86889e0f38bfcd2486e5
Wallis, David <David.Wallis@transunion.com>
2024-10-02 14:06:33 +0100

    various updates

--- src/WeatherForecastTest/Program.cs
+++ src/WeatherForecastTest/Program.cs
@@ -1,4 +1,16 @@
+using WeatherForecastTest.Hosting;
+using WeatherForecastTest.OpenTelemetry;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddWallis2000OpenTelemetry();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -7,14 +19,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsNotProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.AddTraceResponseHeader();
 
 app.UseHttpsRedirection();
 
